eaf8a423142c8192f3032fab9b5832a574954aac
ssbeast <ssbeast007@gmail.com>
2021-06-28 04:07:38 +0530

    create_block defined

--- blockchain.py
+++ blockchain.py
@@ -8,3 +8,11 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
+
+    def create_bock(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
